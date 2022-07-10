ed3b3171a3c569f0b90622e65bbd68ec896eec0b
AlexPodolich <adpodol@gmail.com>
2022-07-10 20:06:20 +0200

    Deleted incorrect UTP task

--- UTP(Java tasks)/assignment-10/src/main/java/eu/glowacki/utp/assignment10/Task10Exception.java
+++
@@ -1,9 +0,0 @@
-package eu.glowacki.utp.assignment10;
-
-import java.sql.SQLException;
-
-public class Task10Exception extends Exception{
-    public Task10Exception(SQLException e) {
-        super(e);
-    }
-}
